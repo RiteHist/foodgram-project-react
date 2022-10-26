5e5cfcf342a29e745972302e4003d6bce1310f06
Широков Анатолий Владимирович <ShirokovAV@beluga.group>
2022-10-27 09:37:45 +1000

    Add endpoints for tags and ingredients.

---
+++ backend/foodgram/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from foods.models import Ingredient, Tag
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = ('id',
+                  'name',
+                  'measurement_unit'
+                  )
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ('id',
+                  'name',
+                  'color',
+                  'slug'
+                  )
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import filters
 
-# Create your views here.
+from foods.models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
+
+
+class CustomSearchFilter(filters.SearchFilter):
+    search_param = 'name'
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    pagination_class = None
+    filter_backends = [CustomSearchFilter]
+    search_fields = ['^name']
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None
